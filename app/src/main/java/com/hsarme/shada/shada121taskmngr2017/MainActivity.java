5e6c67f11193f7310b2e9d8e1e25ac37c2d8da95
shadah15 <shada.hsarme@gmail.com>
2017-09-13 13:23:13 +0300

    bfufju

--- app/src/main/java/com/hsarme/shada/shada121taskmngr2017/MainActivity.java
+++ app/src/main/java/com/hsarme/shada/shada121taskmngr2017/MainActivity.java
@@ -2,6 +2,9 @@ package com.hsarme.shada.shada121taskmngr2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+    {
+        case R.id.mnItmScreen1:
+            Toast.makeText(getBaseContext(), "Hi...Screen1", Toast.LENGTH_LONG).show();
+            break;
+        case R.id.mnItmScreen2:
+            Toast.makeText(getBaseContext(), "Hi...Screen2", Toast.LENGTH_LONG).show();
+            break;
+        case R.id.mnItmScreen3:
+            Toast.makeText(getBaseContext(), "Hi...Screen3", Toast.LENGTH_LONG).show();
+            break;
+        case R.id.mnItmScreen4:
+            Toast.makeText(getBaseContext(), "Hi...Screen4", Toast.LENGTH_LONG).show();
+            break;
+    }
+    return true;
+    }
+
 }
